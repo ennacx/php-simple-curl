d595616a43736be866db4c9a1c509e127b4f9161
Ennacx <enna.cx@gmail.com>
2024-11-19 13:18:37 +0900

    PHPUnit

---
+++ tests/MultiCurlLibTest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Ennacx\SimpleCurl\Test;
+
+use Ennacx\SimpleCurl\Entity\ResponseEntity;
+use Ennacx\SimpleCurl\Enum\HttpStatusCode;
+use Ennacx\SimpleCurl\MultiCurlLib;
+use Ennacx\SimpleCurl\SimpleCurlLib;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * MultiCurlLib Test
+ */
+class MultiCurlLibTest extends TestCase {
+
+    private const TEST_USE_URL1 = 'https://www.google.co.jp';
+    private const TEST_USE_URL2 = 'https://www.yahoo.co.jp';
+    private const TEST_USE_URL3 = 'https://php.net';
+    private const TEST_USE_URL4 = 'https://cakephp.org';
+
+    private MultiCurlLib $lib;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $this->lib = new MultiCurlLib();
+    }
+
+    public function testAddChannel(){
+
+        $sc1 = new SimpleCurlLib(self::TEST_USE_URL1);
+        $sc2 = new SimpleCurlLib(self::TEST_USE_URL2);
+        $sc3 = new SimpleCurlLib(self::TEST_USE_URL3);
+        $sc4 = new SimpleCurlLib(self::TEST_USE_URL4);
+
+        $this->lib
+            ->addChannel($sc1)
+            ->addChannel($sc2)
+            ->addChannel($sc3)
+            ->addChannel($sc4);
+
+        $this->assertEquals(4, count($this->lib->getChannelIds()));
+    }
+
+    public function testSuccessfully(){
+
+        $sc1 = new SimpleCurlLib(self::TEST_USE_URL1);
+        $sc2 = new SimpleCurlLib(self::TEST_USE_URL2);
+        $sc3 = new SimpleCurlLib(self::TEST_USE_URL3);
+        $sc4 = new SimpleCurlLib(self::TEST_USE_URL4);
+
+        $this->lib
+            ->addChannel($sc1)
+            ->addChannel($sc2)
+            ->addChannel($sc3)
+            ->addChannel($sc4);
+
+        $result = array_map(fn(ResponseEntity $v) => $v->http_status_code, $this->lib->exec());
+
+        $this->assertTrue(empty(array_filter($result, fn($v) => ($v !== HttpStatusCode::OK->value && $v !== HttpStatusCode::MOVED_PERMANENTLY->value))));
+    }
+}
---
+++ tests/SimpleCurlLibTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Ennacx\SimpleCurl\Test;
+
+use Ennacx\SimpleCurl\Enum\HttpStatusCode;
+use Ennacx\SimpleCurl\SimpleCurlLib;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * SimpleCurlLib Test
+ */
+class SimpleCurlLibTest extends TestCase {
+
+    private const TEST_USE_URL = 'https://www.google.co.jp';
+
+    private SimpleCurlLib $lib;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $this->lib = new SimpleCurlLib();
+    }
+
+    public function testSetUrl(){
+
+        $this->lib->setUrl(self::TEST_USE_URL);
+
+        $this->assertEquals(self::TEST_USE_URL, $this->lib->getUrl());
+    }
+
+    public function testConnection(){
+
+        $this->lib->setUrl(self::TEST_USE_URL);
+        $entity = $this->lib->exec();
+
+        $this->assertEquals(HttpStatusCode::OK->value, $entity->http_status_code);
+    }
+
+    public function testReturnTransfer(){
+
+        $this->lib->setUrl(self::TEST_USE_URL);
+        $this->lib->setReturnTransfer(true);
+
+        $result = $this->lib->exec();
+
+        $this->assertTrue((!empty($result->responseBody)));
+    }
+}
